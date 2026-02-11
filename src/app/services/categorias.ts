11e5c82ff986608545675f6de051afb34824b2e1
diego.anguiano <diego.anguiano@atmira.com>
2026-02-11 16:39:29 +0100

    Final

--- src/app/services/categorias.ts
+++ src/app/services/categorias.ts
@@ -5,8 +5,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
   providedIn: 'root',
 })
 export class Categorias {
-  //url: string = 'http://localhost:8080/v1/categorias';
-  url: string = 'http://Apirest-books-aws-env-3.eba-ntyg4bif.us-west-2.elasticbeanstalk.com/v1/categorias';
+  url: string = 'http://localhost:8080/v1/categorias';
 
   constructor(private http: HttpClient) {
   }
